9d3988d14453f1c672d8f101650ca170dd827e30
linyangcong <linyc_gd@163.com>
2020-01-20 18:04:56 +0800

    publish0.1

--- build/precache-manifest.4c782f26d65eaa5158ae537bba17fc5c.js
+++ build/precache-manifest.4c782f26d65eaa5158ae537bba17fc5c.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.4a686d48.js"
   },
   {
-    "revision": "d6ea3108cf278aab2972",
-    "url": "./static/js/main.d6ea3108.chunk.js"
+    "revision": "f762fc09375123513489",
+    "url": "./static/js/main.f762fc09.chunk.js"
   },
   {
     "revision": "15d154404f1f8f42c7fd",
     "url": "./static/js/1.15d15440.chunk.js"
   },
   {
-    "revision": "d6ea3108cf278aab2972",
+    "revision": "f762fc09375123513489",
     "url": "./static/css/main.87bc4aed.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "./static/css/1.efbcc488.chunk.css"
   },
   {
-    "revision": "4da2588eaa06aa0aaa14daadffad0e0e",
+    "revision": "fe8088b331791b36092c86d83f55e317",
     "url": "./index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e9c000f8d6453bbafe0bea2e4486fdd4.js"
+  "./precache-manifest.4c782f26d65eaa5158ae537bba17fc5c.js"
 );
 
 workbox.clientsClaim();
